a584f44cde08c141270ebf648a690c9a3475f60d
thomaskavi <thomaskavi2000@gmail.com>
2025-05-21 09:44:53 -0300

    First commit

---
+++ src/main/java/com/thomaskavi/agendaki/AgendakiApplication.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.agendaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomaskavi/agendaki/AgendakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.agendaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
