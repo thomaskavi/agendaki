d10fab356eccb9059e4ae61700c0ebcef652d55e
thomaskavi <thomaskavi2000@gmail.com>
2025-05-21 14:25:50 -0300

    Controllers

---
+++ src/main/java/com/thomaskavi/agendaki/controllers/AppointmentController.java
@@ -0,0 +1,62 @@
+package com.thomaskavi.agendaki.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.thomaskavi.agendaki.dto.AppointmentDTO;
+import com.thomaskavi.agendaki.dto.UpdateAppointmentDTO;
+import com.thomaskavi.agendaki.services.AppointmentService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/appointments")
+public class AppointmentController {
+
+  @Autowired
+  private AppointmentService service;
+
+  @GetMapping
+  public ResponseEntity<List<AppointmentDTO>> findAll() {
+    List<AppointmentDTO> list = service.findAll();
+    return ResponseEntity.ok(list);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<AppointmentDTO> findById(@PathVariable Long id) {
+    AppointmentDTO dto = service.findById(id);
+    return ResponseEntity.ok(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<AppointmentDTO> insert(@Valid @RequestBody AppointmentDTO dto) {
+    dto = service.insert(dto);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(dto.getId()).toUri();
+    return ResponseEntity.created(uri).body(dto);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<AppointmentDTO> update(@PathVariable Long id, @Valid @RequestBody UpdateAppointmentDTO dto) {
+    AppointmentDTO updatedDto = service.update(id, dto);
+    return ResponseEntity.ok(updatedDto);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+}
---
+++ src/main/java/com/thomaskavi/agendaki/controllers/ProfessionalController.java
@@ -0,0 +1,61 @@
+package com.thomaskavi.agendaki.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.thomaskavi.agendaki.dto.ProfessionalDTO;
+import com.thomaskavi.agendaki.services.ProfessionalService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/professionals")
+public class ProfessionalController {
+
+  @Autowired
+  private ProfessionalService service;
+
+  @GetMapping
+  public ResponseEntity<List<ProfessionalDTO>> findAll() {
+    List<ProfessionalDTO> list = service.findAll();
+    return ResponseEntity.ok(list);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<ProfessionalDTO> findById(@PathVariable Long id) {
+    ProfessionalDTO dto = service.findById(id);
+    return ResponseEntity.ok(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<ProfessionalDTO> insert(@Valid @RequestBody ProfessionalDTO dto) {
+    dto = service.insert(dto);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(dto.getId()).toUri();
+    return ResponseEntity.created(uri).body(dto);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<ProfessionalDTO> update(@PathVariable Long id, @Valid @RequestBody ProfessionalDTO dto) {
+    dto = service.update(id, dto);
+    return ResponseEntity.ok(dto);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+}
