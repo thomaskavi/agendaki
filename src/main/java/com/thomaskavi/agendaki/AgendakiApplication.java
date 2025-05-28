e0bb1707b6837c9aad36a827a64fc190c2fdf731
thomaskavi <thomaskavi2000@gmail.com>
2025-05-28 17:11:13 -0300

    First upload test

--- src/main/java/com/thomaskavi/agendaki/AgendakiApplication.java
+++ src/main/java/com/thomaskavi/agendaki/AgendakiApplication.java
@@ -1,13 +1,25 @@
 package com.thomaskavi.agendaki;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thomaskavi.agendaki.services.S3Service;
+
 @SpringBootApplication
-public class AgendakiApplication {
+public class AgendakiApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendakiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\thoma\\Desktop\\print.png");
 	}
 
 }
